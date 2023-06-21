ca6bf3c242b8d0a3287cd63d1685b9ba7558e2a1
= <olimxonnutfullaevich@gmail.com>
2023-06-21 19:48:50 +0500

    DATA Denerics & Implementation

--- AssignmenCheck.API/Program.cs
+++ AssignmenCheck.API/Program.cs
@@ -1,8 +1,17 @@
+using AssignmentCheck.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
+builder.Services.AddDbContext<AssignmentCheckDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddControllers();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
